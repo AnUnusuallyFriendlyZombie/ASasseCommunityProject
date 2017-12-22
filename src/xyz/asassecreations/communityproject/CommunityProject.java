845a60275b8f1bf44083e0aa1a5f6dc22985dd0d
ASasseCreations <ASasseCreations@gmail.com>
2017-12-21 22:33:03 -0500

    Loading grass texture and rendering it

--- src/xyz/asassecreations/communityproject/CommunityProject.java
+++ src/xyz/asassecreations/communityproject/CommunityProject.java
@@ -2,16 +2,31 @@ package xyz.asassecreations.communityproject;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public final class CommunityProject {
 
 	private static final Window window = new Window(1280, 720, "Game");
+	private static BufferedImage image;
 
 	private CommunityProject() {
 
 	}
 
 	public static final void main(final String[] args) {
+
+		try {
+
+			image = ImageIO.read(Class.class.getResourceAsStream("/grass.png"));
+
+		} catch (final IOException e) {
+
+			e.printStackTrace();
+
+		}
 
 		while (true)
 			do {
@@ -36,6 +51,8 @@ public final class CommunityProject {
 
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, window.width, window.height);
+
+		g.drawImage(image, 0, 0, 32, 32, null);
 
 	}
 }
